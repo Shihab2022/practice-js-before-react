638c5bff3dc064f9bdcdeaca4e49585d6e0b2c56
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-03-12 10:55:38 +0600

    first commit

---
+++ module-21.js
@@ -0,0 +1,68 @@
+/************************  multiplay all value in an array by function************************* */
+
+// const numbers = [10,55,888,11,30,155,45,90]
+// function multiplay (numbers){
+// let sum = 0;
+// for (const number of numbers){
+//     sum += number
+// }
+// return sum
+//use for each
+// let sum =0 ;
+// numbers.forEach(number =>sum = sum+number);
+// return sum
+// }
+// console.log(multiplay(numbers));
+
+
+/************************  multiplay all value in an array by for loop and for each************************* */
+// let sum = 0;
+// for (const number of numbers){
+//     sum += number
+// }
+// / let sum =0 ;
+// // numbers.forEach(number =>sum = sum+number);
+// console.log(sum);
+/************************ find the largest value in an array by function************************* */
+
+
+// muloto ay largest  value bar korar jonno agy akta valu kay store koray rakty hobay 
+
+// let numbers = [10,55,888,11,30,155,45,90]
+
+// function biggestNum(numbers){
+//     let largest =numbers[0];
+
+//     // add for 
+
+//     // for(let i=0 ; i<numbers.length; i++){
+//     //     const element =numbers[i];
+//     //     if(element>largest){
+//     //         largest = element
+//     //     }
+//     // }
+
+//     // add for of
+
+//     for(const number of numbers){
+//         const element =number;
+//         if(element>largest){
+//             largest = element
+//         }  
+//     }
+//     return largest
+// }
+// console.log(biggestNum(numbers))
+/************************  multiplay all value in an array by function************************* */
+/************************  multiplay all value in an array by function************************* */
+/************************  multiplay all value in an array by function************************* */
+/************************  multiplay all value in an array by function************************* */
+/************************  multiplay all value in an array by function************************* */
+/************************  multiplay all value in an array by function************************* */
+/************************  multiplay all value in an array by function************************* */
+/************************  multiplay all value in an array by function************************* */
+/************************  multiplay all value in an array by function************************* */
+/************************  multiplay all value in an array by function************************* */
+/************************  multiplay all value in an array by function************************* */
+/************************  multiplay all value in an array by function************************* */
+/************************  multiplay all value in an array by function************************* */

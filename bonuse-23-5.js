4339c1e601c837ccefc12f4aa2f03824233b12c8
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-03-12 22:04:21 +0600

    fivth commit

---
+++ bonuse-23-5.js
@@ -0,0 +1,73 @@
+
+/************************ unlimited vaby akta function kay chality chai taholay************************* */
+
+// function sumUnlimeted(num){ //fun ta unlimeted vaby cholty thakby akta somy ak aka of hoay jabby
+   
+//     let sum =num+1
+//     console.log(sum)
+//     sumUnlimeted(sum)
+   
+// }
+// sumUnlimeted(2)
+
+/************************ akta nirdisto man por jonto chai  taholay************************* */
+//  function sumUnlimeted(num){
+   
+//         let sum =num+1
+//         if(sum >200){
+//             return;
+//         }
+//         console.log(sum)
+//         sumUnlimeted(sum)
+       
+//     }
+//     sumUnlimeted(0)
+
+
+// function sum(i){
+//     // console.log(i)
+//     if(i==1){
+//         return 1 ;
+
+//     }
+//     return i+sum(--i)
+// }
+// console.log(sum(5))
+
+
+/************************ facktorial ************************* */
+
+// function factorial(i){
+// if(i==1){
+//     return 1 ;
+// }
+// return i*factorial(--i)
+// }
+// console.log(factorial(5))
+// console.log(factorial(10))
+
+
+/************************ show that the similar element from nasted array  ************************* */
+
+const phones =[
+    {name:'samsung' ,price:25500 ,color:'black' , model:'m25'},
+    {name:'iphone' ,price:255000 ,color:'gaygreen' , model:'iohone 13pro'},
+    {name:'mi' ,price:5500 ,color:'black' , model:'note 10'},
+    {name:'oppo' ,price:20500 ,color:'white' , model:'m25'},
+    {name:'walton' ,price:25000 ,color:'black' , model:'m25'},
+    {name:'realme' ,price:50500 ,color:'green' , model:'m25'},
+]
+
+// for (const product of phones){
+//     if(product.color.includes('black')){ //ay for ar maddomay jay object gulo ty color black asay tader kay dakbay
+//         console.log(product)
+//     }
+// }
+
+//use Es6 featcher
+
+// phones.forEach(product => {
+//     if(product.color.includes('black')){
+//         console.log(product)
+//     }
+// })

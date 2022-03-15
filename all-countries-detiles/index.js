3d5992f48a4c83112d5d65833504070c680f2513
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-03-15 17:01:34 +0600

    add all countries

---
+++ all-countries-detiles/index.js
@@ -0,0 +1,10 @@
+const backgroudChange=(colorId)=>{
+    console.log(colorId)
+    if(colorId==='white'){
+        document.body.style.backgroundColor='LightCoral'
+    }
+    else if(colorId === 'black'){
+        document.body.style.backgroundColor='black'
+    }
+  
+}
